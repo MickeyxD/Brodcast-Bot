84700baa1b8352d91c20b6f2b219a0cadd6fab8d
♡мιcкεү♡ <88245294+MickeyxD@users.noreply.github.com>
2021-10-02 16:58:49 +0530

    Update bot.py

--- Brodcaster/bot.py
+++ Brodcaster/bot.py
@@ -1,1 +1,10 @@
 
+from telethon import TelegramClient
+from brodcaster.Config import api_hash, api_id, string
+from telethon.sessions import StringSession
+
+if string:
+    bot = TelegramClient(StringSession(string), api_id=api_id, api_hash=api_hash).start()
+    print("client connected")
+else:
+    print("STRING SESSION NOT FOUND") 
